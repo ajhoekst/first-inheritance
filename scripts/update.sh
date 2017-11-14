d67b77556ad26d5d467ac65c4a53694558145b65
Andrew Hoekstra <andrewh@twisthink.com>
2017-11-14 11:55:25 -0500

    Forgot semicolons

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,12 +2,12 @@
 
 CHANGES=$(git diff-index --name-only HEAD --);
 if [ ! -z $CHANGES ]; then
-    echo "Saving changes..."
+    echo "Saving changes...";
     git add .;
     git commit -m "checkpoint";
     echo "Updating code..."
     git rebase master topic;
 else
-    echo "No changes made..."
-    echo "Updating code..."
+    echo "No changes made...";
+    echo "Updating code...";
 fi
