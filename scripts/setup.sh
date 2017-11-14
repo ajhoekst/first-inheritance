8722c2cfd80c4462ad22740dfe0ded5cba27bdff
Andrew Hoekstra <andrewh@twisthink.com>
2017-11-14 12:16:35 -0500

    Fix syntax

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BRANCH=$(git rev-parse --abrev-ref HEAD)
-if [[ "$BRANCH" != "develop"]]
+if [[ "$BRANCH" != "develop" ]]
 then
         echo "Switching to develop branch..."
         git checkout -b develop
