d58fac97cf4a3bfb6878a5fbb2a4afe3863609d7
Andrew Hoekstra <andrewh@twisthink.com>
2017-11-14 11:58:14 -0500

    fogot another semicolon

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BRANCH=$(git rev-parse --abrev-ref HEAD)
+BRANCH=$(git rev-parse --abrev-ref HEAD);
 if [[ "$BRANCH" != "develop"]]; then
         echo "Switching to develop branch...";
         git checkout -b develop;
