f5214dae5ba8ee4cee6ae939423f91c3d27ee630
Andrew Hoekstra <andrewh@twisthink.com>
2017-11-14 12:01:38 -0500

    remove semicolons

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BRANCH=$(git rev-parse --abrev-ref HEAD);
-if [[ "$BRANCH" != "develop"]]; then
-        echo "Switching to develop branch...";
-        git checkout -b develop;
+BRANCH=$(git rev-parse --abrev-ref HEAD)
+if [[ "$BRANCH" != "develop"]]
+then
+        echo "Switching to develop branch..."
+        git checkout -b develop
 fi
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-CHANGES=$(git diff-index --name-only HEAD --);
-if [ ! -z $CHANGES ]; then
-    echo "Saving changes...";
+CHANGES=$(git diff-index --name-only HEAD --)
+if [ ! -z $CHANGES ]
+then
+    echo "Saving changes..."
     git add .;
-    git commit -m "checkpoint";
+    git commit -m "checkpoint"
     echo "Updating code..."
-    git rebase master topic;
+    git rebase master topic
 else
-    echo "No changes made...";
-    echo "Updating code...";
+    echo "No changes made..."
+    echo "Updating code..."
 fi
